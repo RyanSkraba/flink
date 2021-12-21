cff51641c23aeeebf69c156367267ea0164bd635
Ryan Skraba <rskraba@apache.org>
2021-12-21 17:57:41 +0100

    wip: Distribution management

--- flink-python/pyflink/datastream/utils.py
+++ flink-python/pyflink/datastream/utils.py
@@ -7,7 +7,7 @@
 #  "License"); you may not use this file except in compliance
 #  with the License.  You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 #  Unless required by applicable law or agreed to in writing, software
 #  distributed under the License is distributed on an "AS IS" BASIS,
@@ -20,7 +20,7 @@ import datetime
 import pickle
 
 from pyflink.common import Row, RowKind
-from pyflink.common.typeinfo import (RowTypeInfo, TupleTypeInfo, Types,  BasicArrayTypeInfo,
+from pyflink.common.typeinfo import (RowTypeInfo, TupleTypeInfo, Types, BasicArrayTypeInfo,
                                      PrimitiveArrayTypeInfo, MapTypeInfo, ListTypeInfo,
                                      ObjectArrayTypeInfo, ExternalTypeInfo)
 from pyflink.java_gateway import get_gateway
